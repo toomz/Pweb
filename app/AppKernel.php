2fd39ce00af3b6e0ad76b9e9c108b1a94e1c6a56
THEUIL <theuil@theuil-Latitude-E5520.(none)>
2013-04-03 17:09:02 +0200

    e

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Pweb\UserBundle\PwebUserBundle(),
+            new Pweb\CompteBundle\PwebCompteBundle(),
+            new Pweb\AccueilBundle\PwebAccueilBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
