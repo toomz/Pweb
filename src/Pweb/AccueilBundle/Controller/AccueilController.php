71053529cc84169e8ec4f7d34eaddfb48000c382
THEUIL <theuil@theuil-Latitude-E5520.(none)>
2013-04-03 16:59:06 +0200

    debut

---
+++ src/Pweb/AccueilBundle/Controller/AccueilController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pweb\AccueilBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AccueilController extends Controller{
+
+	public function indexAction(){
+		return $this->render('PwebAccueilBundle:Accueil:index.html.twig');
+	}
+
+}
