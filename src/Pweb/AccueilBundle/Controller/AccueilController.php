b233262b1cb56b44521b01e5397fc4f3d151b86e
Toomz <eleonore.pirri@gmail.com>
2013-04-23 22:38:54 +0200

    un tout petit peu de mise en forme

--- src/Pweb/AccueilBundle/Controller/AccueilController.php
+++ src/Pweb/AccueilBundle/Controller/AccueilController.php
@@ -4,9 +4,11 @@ namespace Pweb\AccueilBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AccueilController extends Controller{
+class AccueilController extends Controller
+{
 
-	public function indexAction(){
+	public function indexAction()
+  {
 		return $this->render('PwebAccueilBundle:Accueil:index.html.twig');
 	}
 
