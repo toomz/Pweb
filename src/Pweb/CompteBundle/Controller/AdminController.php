ac2769da29fe4fd3ac242d87832a8b3cc1793d8d
THEUIL <theuil@theuil-Latitude-E5520.(none)>
2013-04-24 15:32:07 +0200

    user et compte

---
+++ src/Pweb/CompteBundle/Controller/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pweb\CompteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller{
+
+	public function indexAction(){
+
+		return $this->render('PwebCompteBundle:Admin:index.html.twig');
+		
+	}
+
+}
---
+++ src/Pweb/CompteBundle/Controller/UtilController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pweb\CompteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use Pweb\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class UtilController extends Controller{
+
+	public function indexAction(){
+
+		
+		
+		$em = $this->getDoctrine()->getEntityManager();
+    	$user_list = $em->getRepository("PwebUserBundle:User")->findAll();
+    	return $this->render('PwebCompteBundle:Util:index.html.twig',array('user_list'=>$user_list));
+		
+	}
+
+}
