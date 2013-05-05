f3659048b332f91d20f82c83529b23fcb78afca6
THEUIL <theuil@theuil-Latitude-E5520.(none)>
2013-05-05 19:11:47 +0200

    changements

--- src/Pweb/CompteBundle/DataFixtures/ORM/Commandes.php
+++ src/Pweb/CompteBundle/DataFixtures/ORM/Commandes.php
@@ -91,7 +91,7 @@ class Commandes extends AbstractFixture implements OrderedFixtureInterface{
   }
 
   public function getOrder(){
-    return 2; // the order in which fixtures will be loaded
+    return 3; // the order in which fixtures will be loaded
   }
 
 }
